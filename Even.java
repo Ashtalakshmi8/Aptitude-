c0a557be500b9ffb2cf24e605800bde402f9b3d0
Ashtalakshmi8 <ASHTALAKSHMI8>
2021-12-26 20:33:32 +0530

    if-else-if-conditional

---
+++ Even.java
@@ -0,0 +1,12 @@
+class Even{
+      public static void main(String [] args ){
+      int no=12;
+      if( no%2==0){
+      System.out.println( "no is Even ");
+      }
+else
+{
+System.out.println( "no is odd ");
+}
+}
+}
---
+++ Evenoddd.java
@@ -0,0 +1,19 @@
+class Evenoddd{
+public static void main(String [] args){
+
+int num=1;
+
+for(num=1;num<=10;num=num+1){
+
+if(num %2 ==0){
+
+System.out.println("even"+ " " +num);
+
+}else
+{
+System.out.println("odd"+ " " +num);
+}
+
+}
+}
+}
